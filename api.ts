a7d7ee60a66ff0036e624f5e1aef0e693fa0c573
svolkov <svolkov@everpoint.ru>
2020-01-13 02:47:10 +0300

    feat: initial commit (add primitive swagger 3.0 parser)

---
+++ api.ts
@@ -0,0 +1,245 @@
+
+
+export type AuthUser = {
+  username:string,
+  password:string,
+}
+export enum Kind {
+  COMPANY = "COMPANY",
+  PERSONAL = "PERSONAL",
+  FREELANCE = "FREELANCE",
+  OPEN_SOURCE = "OPEN_SOURCE" 
+ }
+export type Job = {
+  id:string,
+  kind:Kind,
+  name?:string,
+  link?:string,
+  github?:string,
+  npm?:string,
+  isTool?:boolean,
+  address?:string,
+}
+export type NewJob = {
+  kind:Kind,
+  name?:string,
+  link?:string,
+  github?:string,
+  npm?:string,
+  isTool?:boolean,
+  address?:string,
+}
+export type Project = {
+  id:string,
+  year:number,
+  description:string,
+  job?:Job,
+  name?:string,
+  notImportant?:boolean,
+  prefix?:string,
+  tags:string[],
+  teamSize?:string,
+}
+export type NewProject = {
+  year:number,
+  description:string,
+  name?:string,
+  notImportant?:boolean,
+  prefix?:string,
+  tags:string[],
+  teamSize?:string,
+  job:string,
+}
+export type User = {
+  id:string,
+  username:string,
+}
+export type UserUpdate = {
+  id?:string,
+  username?:string,
+}
+
+
+export type ApiParams = Omit<RequestInit, "body" | "method">
+
+type ApiConfig<SecurityDataType> = {
+  securityWorker?: (securityData: SecurityDataType) => ApiParams
+}
+
+
+
+export class Api<SecurityDataType> {
+  
+  public baseUrl = "https://localhost:3000/api/v1";
+  public title = "ts-mongodb-server";
+  public version = "1.0.0";
+  
+  private securityData: SecurityDataType;
+  private securityWorker: ApiConfig<SecurityDataType>["securityWorker"] = (() => {}) as any
+  private defaultRequestParams: ApiParams = {
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+  }
+
+  
+  constructor({ securityWorker }: ApiConfig<SecurityDataType>) {
+    if (securityWorker) {
+      this.securityWorker = securityWorker;
+    }
+  }
+  public setSecurityData = (data: SecurityDataType) => {
+    this.securityData = data
+  }
+
+  
+
+  /**
+   * @tags Auth
+   * @name Login
+   */
+  login = (data: AuthUser, params: ApiParams = {}): Promise<string> =>
+    fetch(`${this.baseUrl}/auth`, {
+      ...this.defaultRequestParams,
+      method: 'POST',
+      body: JSON.stringify(data),
+      ...params,
+    }).then(response => response.json()) 
+    
+
+  /**
+   * @tags Auth
+   * @name Refresh
+   * @security true
+   */
+  refresh = (params: ApiParams = {}): Promise<string> =>
+    fetch(`${this.baseUrl}/auth/refresh`, {
+      ...this.defaultRequestParams,
+      method: 'POST',
+      body: null,
+      ...params,
+      ...(this.securityWorker(this.securityData))
+    }).then(response => response.json()) 
+    
+
+  /**
+   * @tags Jobs
+   * @name GetJob
+   * @security true
+   */
+  getJob = (id: string, params: ApiParams = {}): Promise<Job> =>
+    fetch(`${this.baseUrl}/jobs/${id}`, {
+      ...this.defaultRequestParams,
+      method: 'GET',
+      body: null,
+      ...params,
+      ...(this.securityWorker(this.securityData))
+    }).then(response => response.json()) 
+    
+
+  /**
+   * @tags Jobs
+   * @name AddJob
+   * @security true
+   */
+  addJob = (data: NewJob, params: ApiParams = {}): Promise<string> =>
+    fetch(`${this.baseUrl}/jobs`, {
+      ...this.defaultRequestParams,
+      method: 'POST',
+      body: JSON.stringify(data),
+      ...params,
+      ...(this.securityWorker(this.securityData))
+    }).then(response => response.json()) 
+    
+
+  /**
+   * @tags Projects
+   * @name GetProjects
+   */
+  getProjects = (params: ApiParams = {}): Promise<Project[]> =>
+    fetch(`${this.baseUrl}/projects`, {
+      ...this.defaultRequestParams,
+      method: 'GET',
+      body: null,
+      ...params,
+    }).then(response => response.json()) 
+    
+
+  /**
+   * @tags Projects
+   * @name AddProjects
+   * @security true
+   */
+  addProjects = (data: NewProject, params: ApiParams = {}): Promise<string> =>
+    fetch(`${this.baseUrl}/projects`, {
+      ...this.defaultRequestParams,
+      method: 'POST',
+      body: JSON.stringify(data),
+      ...params,
+      ...(this.securityWorker(this.securityData))
+    }).then(response => response.json()) 
+    
+
+  /**
+   * @tags Users
+   * @name GetUsers
+   * @security true
+   */
+  getUsers = (params: ApiParams = {}): Promise<User[]> =>
+    fetch(`${this.baseUrl}/users`, {
+      ...this.defaultRequestParams,
+      method: 'GET',
+      body: null,
+      ...params,
+      ...(this.securityWorker(this.securityData))
+    }).then(response => response.json()) 
+    
+
+  /**
+   * @tags Users
+   * @name AddUser
+   * @security true
+   */
+  addUser = (data: AuthUser, params: ApiParams = {}): Promise<User> =>
+    fetch(`${this.baseUrl}/users`, {
+      ...this.defaultRequestParams,
+      method: 'POST',
+      body: JSON.stringify(data),
+      ...params,
+      ...(this.securityWorker(this.securityData))
+    }).then(response => response.json()) 
+    
+
+  /**
+   * @tags Users
+   * @name DeleteUser
+   * @security true
+   */
+  deleteUser = (id: string, params: ApiParams = {}): Promise<any> =>
+    fetch(`${this.baseUrl}/users/${id}`, {
+      ...this.defaultRequestParams,
+      method: 'DELETE',
+      body: null,
+      ...params,
+      ...(this.securityWorker(this.securityData))
+    }).then(response => response.json()) 
+    
+
+  /**
+   * @tags Users
+   * @name UpdateUser
+   * @security true
+   */
+  updateUser = (id: string, data: UserUpdate, params: ApiParams = {}): Promise<User> =>
+    fetch(`${this.baseUrl}/users/${id}`, {
+      ...this.defaultRequestParams,
+      method: 'PATCH',
+      body: JSON.stringify(data),
+      ...params,
+      ...(this.securityWorker(this.securityData))
+    }).then(response => response.json()) 
+    
+}
---
+++ index.js
@@ -0,0 +1,166 @@
+const fs = require("fs");
+const ts = require("typescript");
+const _ = require("lodash");
+const { info, paths, servers, components } = require('./swagger.json');
+
+const getTypeNameFromRef = (ref) => _.last(_.split(ref, '/'))
+
+const getEnumTypeProps = (_enum) => {
+  return _enum.map(key => `  ${key} = "${key}"`).join(',\n')
+}
+
+const specificObjectTypes = {
+  'array': ({ items }) => {
+    switch(true) {
+      case (!!items.type): return `${items.type}[]`;
+      case (!!items.$ref): return `${getTypeNameFromRef(items.$ref)}[]`;
+      default: return `any[]`
+    }
+  }
+}
+
+const getType = (property) => {
+  const func = specificObjectTypes[property.type] || (() => property.type)
+  return property["$ref"] ? getTypeNameFromRef(property["$ref"]) : func(property)
+}
+
+const getObjectTypeProps = (properties) => {
+  return _.map(properties, (property, name) => {
+    return `  ${name}${property.nullable ? '?' : ''}:${getType(property)},\n`
+  }).join('')
+}
+
+const parseSchema = ({ properties, type, enum: _enum }, typeName) => {
+
+  const typeContent =
+    _enum ? getEnumTypeProps(_enum) :
+    properties ? getObjectTypeProps(properties) : type
+  const getTypeData = {
+    ['enum']: `enum ${typeName} {\r\n${typeContent} \r\n }`,
+    ['type']: `type ${typeName} = {\r\n${typeContent}}`
+  }
+
+
+  return `export ${getTypeData[_enum ? 'enum' : 'type']}\n`
+}
+
+
+const SchemasTypes = _.map(components.schemas, (schema, typeName) => {
+
+  return parseSchema(schema, typeName)
+}).join('')
+
+const server = servers[0]
+
+let Types = `
+${SchemasTypes}
+
+export type ApiParams = Omit<RequestInit, "body" | "method">
+
+type ApiConfig<SecurityDataType> = {
+  securityWorker?: (securityData: SecurityDataType) => ApiParams
+}
+
+`
+
+const ApiProperties = `
+  public baseUrl = "${server.url}";
+  public title = "${info.title}";
+  public version = "${info.version}";
+  
+  private securityData: SecurityDataType;
+  private securityWorker: ApiConfig<SecurityDataType>["securityWorker"] = (() => {}) as any
+  private defaultRequestParams: ApiParams = {
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+  }
+`
+
+const getTypeFromRequestInfo = requestInfo => {
+
+  const schema = _.get(requestInfo, 'content["application/json"].schema')
+  if (schema) {
+    return getType(schema);
+  }
+
+  return 'any';
+}
+
+const findSuccessResponse = (responses) => {
+  return _.find(responses, (v, status) => +status >= 200 && +status < 300)
+}
+
+const routesByTags = Object.entries(paths)
+  .reduce((routes, [route, requestInfoByMethodsMap]) => {
+
+    return routes +
+    _.map(requestInfoByMethodsMap, ({
+      operationId,
+      requestBody,
+      security,
+      parameters,
+      tags,
+      responses,
+    }, method) => {
+        const hasSecurity = !!security.length
+        const hasParameters = parameters && parameters.length;
+        const args = [
+          ...(hasParameters ? parameters.map(param => param.in === 'path' && `${param.name}: ${param.schema.type}`) : []),
+          requestBody ? `data: ${getTypeFromRequestInfo(requestBody)}` : '',
+          'params: ApiParams = {}'
+        ].filter(Boolean)
+        const successResponse = findSuccessResponse(responses);
+
+        const comments = [
+          tags && tags.length && `@tags ${tags.join(', ')}`,
+          `@name ${operationId}`,
+          hasSecurity ? `@security true` : ''
+        ].filter(Boolean).map(c => '   * ' + c).join('\r\n');
+        return `
+
+  /**
+${comments}
+   */
+  ${_.camelCase(operationId)} = (${args.join(', ')}): Promise<${getTypeFromRequestInfo(successResponse)}> =>
+    fetch(\`\${this.baseUrl}${route.replace(/{/g, '${')}\`, {
+      ...this.defaultRequestParams,
+      method: '${_.upperCase(method)}',
+      body: ${requestBody ? 'JSON.stringify(data)' : 'null'},
+      ...params,${hasSecurity ? '\r\n      ...(this.securityWorker(this.securityData))' : ''}
+    }).then(response => response.json()) 
+    `}).join('')
+  }, '')
+
+
+const sourceFile = `
+${Types}
+
+export class Api<SecurityDataType> {
+  ${ApiProperties}
+  
+  constructor({ securityWorker }: ApiConfig<SecurityDataType>) {
+    if (securityWorker) {
+      this.securityWorker = securityWorker;
+    }
+  }
+  public setSecurityData = (data: SecurityDataType) => {
+    this.securityData = data
+  }
+
+  ${routesByTags}
+}
+`
+
+fs.writeFile("api.ts", sourceFile, () => {
+
+})
+
+// const program = require('commander');
+ 
+// program
+//   .option('-o, --output', 'output folder')
+//   .option('-p, --path <path>', 'path to swagger.json');
