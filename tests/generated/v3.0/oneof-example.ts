4aef4282f0ba98728e7872b57371174bd47db2fd
svolkov <svolkov@everpoint.ru>
2020-02-12 21:31:32 +0300

    test: add examples with allOf, oneOf schemas

---
+++ tests/generated/v3.0/oneof-example.ts
@@ -0,0 +1,114 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/*
+* ---------------------------------------------------------------
+* ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+* ##                                                           ##
+* ## AUTHOR: acacode                                           ##
+* ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+* ---------------------------------------------------------------
+*/
+
+
+export interface Dog {
+  bark?: boolean;
+  breed?: "Dingo" | "Husky" | "Retriever" | "Shepherd";
+}
+
+export interface Cat {
+  hunts?: boolean;
+  age?: number;
+}
+
+export type RequestParams = Omit<RequestInit, "body" | "method"> & {
+  secure?: boolean;
+}
+
+type ApiConfig<SecurityDataType> = {
+  baseUrl?: string,
+  baseApiParams?: RequestParams,
+  securityWorker?: (securityData: SecurityDataType) => RequestParams,
+}
+
+
+
+export class Api<SecurityDataType> {
+  
+  public baseUrl = "";
+  public title = "Oneof Example";
+  public version = "1.0.0";
+
+  private securityData: SecurityDataType = (null as any);
+  private securityWorker: ApiConfig<SecurityDataType>["securityWorker"] = (() => {}) as any
+  
+  private baseApiParams: RequestParams = {
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+  }
+
+  constructor({ baseUrl,baseApiParams,securityWorker, }: ApiConfig<SecurityDataType> = {}) {
+    this.baseUrl = baseUrl || this.baseUrl;
+    this.baseApiParams = baseApiParams || this.baseApiParams;
+    this.securityWorker = securityWorker || this.securityWorker;
+  }
+
+  public setSecurityData = (data: SecurityDataType) => {
+    this.securityData = data
+  }
+
+
+  private mergeRequestOptions(params: RequestParams, securityParams?: RequestParams): RequestParams {
+    return {
+      ...this.baseApiParams,
+      ...params,
+      ...(securityParams || {}),
+      headers: {
+        ...(this.baseApiParams.headers || {}),
+        ...(params.headers || {}),
+        ...((securityParams && securityParams.headers) || {})
+      }
+    }
+  }
+  
+  private safeParseResponse = <T = any>(response: Response): Promise<T> =>
+    response.json()
+      .then(data => data)
+      .catch(e => response.text);
+  
+  public request = <T = any>(
+    path: string,
+    method: string,
+    { secure, ...params }: RequestParams = {},
+    body?: any,
+    secureByDefault?: boolean,
+  ): Promise<T> =>
+    fetch(`${this.baseUrl}${path}`, {
+      // @ts-ignore
+      ...this.mergeRequestOptions(params, (secureByDefault || secure) && this.securityWorker(this.securityData)),
+      method,
+      body: body ? JSON.stringify(body) : null,
+    }).then(async response => {
+      const data = await this.safeParseResponse<T>(response);
+      if (!response.ok) throw data
+      return data
+    })
+
+
+
+  pets = {
+
+
+    /**
+    * @name patch
+    * @request PATCH:/pets
+    */
+    patch: (data: Cat | Dog, params?: RequestParams) =>
+      this.request<any>(`/pets`, "PATCH", params, data),
+  }
+
+}
