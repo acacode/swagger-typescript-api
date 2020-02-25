9091b5533fb0467606f343367a8ba30de241123e
svolkov <svolkov@everpoint.ru>
2020-02-26 02:44:02 +0300

    fix: TPromise type

--- tests/generated/v2.0/petstore-minimal.ts
+++ tests/generated/v2.0/petstore-minimal.ts
@@ -28,7 +28,7 @@ type ApiConfig<SecurityDataType> = {
 }
 
 /** Overrided Promise type. Needs for additional typings of `.catch` callback */
-type TPromise<ResolveType, RejectType = any> = {
+type TPromise<ResolveType, RejectType = any> = Omit<Promise<ResolveType>, "then" | "catch"> & {
   then<TResult1 = ResolveType, TResult2 = never>(onfulfilled?: ((value: ResolveType) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: RejectType) => TResult2 | PromiseLike<TResult2>) | undefined | null): TPromise<TResult1 | TResult2, RejectType>;
   catch<TResult = never>(onrejected?: ((reason: RejectType) => TResult | PromiseLike<TResult>) | undefined | null): TPromise<ResolveType | TResult, RejectType>;
 }
