72733ee6d2635b09d42346ab42ddffa9eee27de7
Filimonov Andrey <andrefilimono@gmail.com>
2020-06-26 23:07:15 +0200

    Custom template (#60)
    * feat: allow to provide custom templates
    
    * docs: update readme
    
    * internal: add manual test script for custom templates option
    
    * feat: resolving paths to templates
    
    * internal: adjust test for custom templates
    
    Co-authored-by: svolkov <svolkov@everpoint.ru>
    Co-authored-by: andrefilimono <andrefilimono@gmail.com>

--- src/files.js
+++ src/files.js
@@ -2,21 +2,15 @@ const _ = require("lodash");
 const fs = require("fs");
 const { resolve } = require("path");
 
-const getFileContent = path =>
-  fs.readFileSync(path, { encoding: 'UTF-8' })
+const getFileContent = (path) => fs.readFileSync(path, { encoding: "UTF-8" });
 
-const pathIsExist = path =>
-  path && fs.existsSync(path)
+const pathIsExist = (path) => path && fs.existsSync(path);
 
 const createFile = (pathTo, fileName, content) =>
-  fs.writeFileSync(resolve(__dirname, pathTo, `./${fileName}`), content, _.noop)
-
-const getTemplate = templateName =>
-  getFileContent(resolve(__dirname, `./templates/${templateName}.mustache`))                       
+  fs.writeFileSync(resolve(__dirname, pathTo, `./${fileName}`), content, _.noop);
 
 module.exports = {
-  getTemplate,
   createFile,
   pathIsExist,
   getFileContent,
-}
+};
---
+++ tests/spec/templates/schema.ts
@@ -0,0 +1,96 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export interface Pet {
+  id: number;
+  name: string;
+  tag?: string;
+  multiple?: string | number;
+}
+
+export type RequestParams = Omit<RequestInit, "body" | "method"> & {
+  secure?: boolean;
+};
+
+type ApiConfig<SecurityDataType> = {
+  baseUrl?: string;
+  baseApiParams?: RequestParams;
+  securityWorker?: (securityData: SecurityDataType) => RequestParams;
+};
+
+const enum BodyType {
+  Json,
+}
+
+class HttpClient<SecurityDataType> {
+  public baseUrl: string = "http://petstore.swagger.io/api";
+  private securityData: SecurityDataType = null as any;
+  private securityWorker: ApiConfig<SecurityDataType>["securityWorker"] = (() => {}) as any;
+
+  private baseApiParams: RequestParams = {
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+  };
+
+  constructor({ baseUrl, baseApiParams, securityWorker }: ApiConfig<SecurityDataType> = {}) {
+    this.baseUrl = baseUrl || this.baseUrl;
+    this.baseApiParams = baseApiParams || this.baseApiParams;
+    this.securityWorker = securityWorker || this.securityWorker;
+  }
+
+  public setSecurityData = (data: SecurityDataType) => {
+    this.securityData = data;
+  };
+
+  private bodyFormatters: Record<BodyType, (input: any) => any> = {
+    [BodyType.Json]: JSON.stringify,
+  };
+
+  private mergeRequestOptions(params: RequestParams, securityParams?: RequestParams): RequestParams {
+    return {
+      ...this.baseApiParams,
+      ...params,
+      ...(securityParams || {}),
+      headers: {
+        ...(this.baseApiParams.headers || {}),
+        ...(params.headers || {}),
+        ...((securityParams && securityParams.headers) || {}),
+      },
+    };
+  }
+
+  private safeParseResponse = <T = any, E = any>(response: Response): Promise<T> =>
+    response
+      .json()
+      .then((data) => data)
+      .catch((e) => response.text);
+
+  public request = <T = any, E = any>(
+    path: string,
+    method: string,
+    { secure, ...params }: RequestParams = {},
+    body?: any,
+    bodyType?: BodyType,
+    secureByDefault?: boolean,
+  ): Promise<T> =>
+    fetch(`${this.baseUrl}${path}`, {
+      // @ts-ignore
+      ...this.mergeRequestOptions(params, (secureByDefault || secure) && this.securityWorker(this.securityData)),
+      method,
+      body: body ? this.bodyFormatters[bodyType || BodyType.Json](body) : null,
+    }).then(async (response) => {
+      const data = await this.safeParseResponse<T, E>(response);
+      if (!response.ok) throw data;
+      return data;
+    });
+}
+
+export class Api<SecurityDataType = any> extends HttpClient<SecurityDataType> {
+  pets = {
+    petsList: (params?: RequestParams) => this.request<Pet[], any>(`/pets`, "GET", params),
+  };
+}
