0c13799a2b6713d389f3230ac9e5b857b73db8cc
svolkov <svolkov@everpoint.ru>
2021-01-22 12:15:30 +0300

    feat: --clean-output option, fix: output folder creation #137 issue

--- src/files.js
+++ src/files.js
@@ -5,6 +5,34 @@ const { filePrefix } = require("./filePrefix");
 
 const getFileContent = (path) => fs.readFileSync(path, { encoding: "UTF-8" });
 
+const pathIsDir = (path) => {
+  if (!path) return false;
+
+  try {
+    const stat = fs.statSync(path);
+    return stat.isDirectory();
+  } catch (e) {
+    return false;
+  }
+};
+
+const removeDir = (path) => {
+  try {
+    fs.rmdirSync(path, { recursive: true });
+  } catch (e) {}
+};
+
+const createDir = (path) => {
+  try {
+    fs.mkdirSync(path);
+  } catch (e) {}
+};
+
+const cleanDir = (path) => {
+  removeDir(path);
+  createDir(path);
+};
+
 const pathIsExist = (path) => path && fs.existsSync(path);
 
 const createFile = (pathTo, fileName, content) =>
@@ -12,6 +40,10 @@ const createFile = (pathTo, fileName, content) =>
 
 module.exports = {
   createFile,
+  pathIsDir,
+  cleanDir,
   pathIsExist,
+  createDir,
+  removeDir,
   getFileContent,
 };
