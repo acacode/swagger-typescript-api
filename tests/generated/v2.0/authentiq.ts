6427aa3463882c347d1382eb01d726d900eafa3d
svolkov <svolkov@everpoint.ru>
2021-04-05 10:42:06 +0300

    bump: up version to 8.0.2; fix: `format` option in `fetch` http client

--- tests/generated/v2.0/authentiq.ts
+++ tests/generated/v2.0/authentiq.ts
@@ -218,7 +218,7 @@ export class HttpClient<SecurityDataType = unknown> {
     const requestParams = this.mergeRequestParams(params, secureParams);
     const queryString = query && this.toQueryString(query);
     const payloadFormatter = this.contentFormatters[type || ContentType.Json];
-    const responseFormat = format && requestParams.format;
+    const responseFormat = format || requestParams.format;
 
     return this.customFetch(`${baseUrl || this.baseUrl || ""}${path}${queryString ? `?${queryString}` : ""}`, {
       ...requestParams,
--- tests/spec/js/schema.js
+++ tests/spec/js/schema.js
@@ -68,7 +68,7 @@ export class HttpClient {
       const requestParams = this.mergeRequestParams(params, secureParams);
       const queryString = query && this.toQueryString(query);
       const payloadFormatter = this.contentFormatters[type || ContentType.Json];
-      const responseFormat = format && requestParams.format;
+      const responseFormat = format || requestParams.format;
       return this.customFetch(`${baseUrl || this.baseUrl || ""}${path}${queryString ? `?${queryString}` : ""}`, {
         ...requestParams,
         headers: {
