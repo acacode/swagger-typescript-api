02727395501766c914177ce1e74dc2421a1ff3b4
Sergey S. Volkov <js2me@outlook.com>
2021-05-01 13:42:22 +0300

    Release 9.0.1 (bugfix) (#248)
    * docs: update mass media in README
    
    * internal: improve test schemas validation using typescript
    
    * refactor: custom npm scripts runner
    
    * fix: problem with --axios option (body keys) (thanks @Nihisil)
    
    * bump: up version to 9.0.1

--- tests/helpers/validateGeneratedModule.js
+++ tests/helpers/validateGeneratedModule.js
@@ -1,42 +1,66 @@
-const { relative } = require("path");
-const fs = require("fs");
-const tsc = require("typescript");
+const ts = require("typescript");
+const tsconfig = require("../../tsconfig.json");
 
+/** @type {ts.CompilerOptions} */
 const compilerOptions = {
+  allowJs: true,
   noEmitOnError: true,
   noImplicitAny: true,
-  target: tsc.ScriptTarget.ES2018,
-  module: tsc.ModuleKind.CommonJS,
+  target: ts.ScriptTarget.ES2018,
+  module: ts.ModuleKind.CommonJS,
   strict: true,
+  alwaysStrict: true,
+  noEmit: true,
 };
 
-const getDiagnosticsFromPath = (pathToFile) =>
-  tsc.createProgram([pathToFile], compilerOptions).emit(void 0, void 0, void 0, true).diagnostics;
-// TODO: that's faster but return 0 error kind of "Cannot find name ''"
-// tsc.transpileModule(fs.readFileSync(pathToFile).toString(), {
-//   compilerOptions,
-//   reportDiagnostics: true,
-//   moduleName:"TEST",
-//   fileName: relative('', pathToFile),
-// }).diagnostics
+function compile(fileNames) {
+  console.log(`compiling ${fileNames.join(", ")}`);
+
+  let program = ts.createProgram(fileNames, compilerOptions);
+  let emitResult = program.emit();
+
+  let allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+
+  allDiagnostics.forEach((diagnostic) => {
+    if (diagnostic.file) {
+      let { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start);
+      let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+      console.error(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+    } else {
+      console.error(ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n"));
+    }
+  });
+
+  let exitCode = allDiagnostics.length || emitResult.emitSkipped ? 1 : 0;
+
+  if (exitCode) {
+    console.log(`Process exiting with code '${exitCode}'.`);
+    process.exit(exitCode);
+  }
+
+  return {
+    code: exitCode,
+    diagnostics: allDiagnostics,
+  };
+}
 
 module.exports = ({ pathToFile }) => {
-  const diagnostics = getDiagnosticsFromPath(pathToFile);
-  const relativePathToFile = relative("", pathToFile);
+  const { diagnostics, code } = compile([pathToFile]);
+  // const relativePathToFile = relative("", pathToFile);
 
-  console.log(`validating ${relativePathToFile}: errors ${diagnostics.length}`);
-  diagnostics.forEach(({ messageText, file, start }) => {
-    var message = tsc.flattenDiagnosticMessageText(messageText, "\n");
-    if (!file) {
-      console.error(`${relativePathToFile}\r\n`, message);
-      return;
-    }
-    var { line, character } = file.getLineAndCharacterOfPosition(start);
-    console.error(
-      `${relativePathToFile}\r\n`,
-      `${file.fileName} (${line + 1},${character + 1}): ${message}`,
-    );
-  });
+  // console.log(`validating ${relativePathToFile}: errors ${diagnostics.length}`);
+  // diagnostics.forEach(({ messageText, file, start }) => {
+  //   var message = ts.flattenDiagnosticMessageText(messageText, "\n");
+  //   if (!file) {
+  //     console.error(`${relativePathToFile}\r\n`, message);
+  //     return;
+  //   }
+  //   var { line, character } = file.getLineAndCharacterOfPosition(start);
+  //   console.error(
+  //     `${relativePathToFile}\r\n`,
+  //     `${file.fileName} (${line + 1},${character + 1}): ${message}`,
+  //   );
+  // });
 
   return diagnostics;
 };
