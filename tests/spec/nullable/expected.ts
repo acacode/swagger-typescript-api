be3b6088201d1c6146535beefbf1230764c4b5e2
js2me <js2me@outlook.com>
2022-10-29 12:56:34 +0600

    chore: add schema for nullable fields\objects

---
+++ tests/spec/nullable/expected.ts
@@ -0,0 +1,29 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export type MyObject = {
+  id: string;
+  name: string;
+} | null;
+
+export interface TestObject {
+  stringMaybeUndefined?: string;
+  stringMaybeNullA: string | null;
+  stringMaybeNullB: null;
+  stringMaybeNullAndUndefined?: null;
+  otherObjectMaybeUndefined?: OtherObject;
+  otherObjectMaybeNullA: OtherObject | null;
+  otherObjectMaybeNullB: OtherObject | null;
+  otherObjectMaybeNullC: OtherObject | null;
+  otherObjectMaybeNullD: OtherObject | null | (OtherObject & null);
+}
+
+export type OtherObject = object;
