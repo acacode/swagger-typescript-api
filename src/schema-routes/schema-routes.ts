f8b8d185e1a3a3685d1267f9363fad3b7b29c6e1
Amir Hoseinian <hoseinian.amir@gmail.com>
2024-02-22 14:59:44 +0100

    Fix route param parsing when the param begins with just one letter
    For example if the param was i_key the current regex would not find it as a variable. it was only working for the params with more than one letter before either `.` `-` or `_`. the fix should allow to find params with only one letter at begninng.

--- src/schema-routes/schema-routes.ts
+++ src/schema-routes/schema-routes.ts
@@ -96,7 +96,7 @@ export class SchemaRoutes {
       originalRouteName;
 
     const pathParamMatches = (routeName || "").match(
-      /({(([A-z]){1}([a-zA-Z0-9]-?_?\.?)+)([0-9]+)?})|(:(([A-z]){1}([a-zA-Z0-9]-?_?\.?)+)([0-9]+)?:?)/g,
+      /({(([A-z]){1}([a-zA-Z0-9-_.]-?_?\.?)+)([0-9]+)?})|(:(([A-z]){1}([a-zA-Z0-9-_.]-?_?\.?)+)([0-9]+)?:?)/g,
     );
 
     // used in case when path parameters is not declared in requestInfo.parameters ("in": "path")
