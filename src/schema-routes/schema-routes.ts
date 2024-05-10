7edabde8df5337e148e3a081d0b39ef463644f74
Amir Hoseinian <hoseinian.amir@gmail.com>
2024-05-10 17:01:28 +0200

    refactor: improve regex for route param parsing
    accourding to this suggestion  https://github.com/acacode/swagger-typescript-api/pull/686#issuecomment-2084927918

--- src/schema-routes/schema-routes.ts
+++ src/schema-routes/schema-routes.ts
@@ -96,7 +96,7 @@ export class SchemaRoutes {
       originalRouteName;
 
     const pathParamMatches = (routeName || "").match(
-      /({[a-zA-Z]([a-zA-Z0-9-_.])*})|(:[a-zA-Z]([-_.]?[a-zA-Z0-9-_.])*:?)/g,
+      /({[a-zA-Z]([-_.]*[a-zA-Z0-9])*})|(:[a-zA-Z]([-_.]*[a-zA-Z0-9])*:?)/g,
     );
 
     // used in case when path parameters is not declared in requestInfo.parameters ("in": "path")
