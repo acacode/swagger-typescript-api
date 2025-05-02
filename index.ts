519e55f0f1e35f14a9f9f994fd8dc159a512fafd
Sora Morimoto <sora@morimoto.io>
2025-05-02 19:39:19 +0900

    Integrate `generateCommand` arguments and run method into main command
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- index.ts
+++ index.ts
@@ -343,6 +343,8 @@ const main = defineCommand({
     description: packageJson.description,
     version: packageJson.version,
   },
+  args: generateCommand.args,
+  run: generateCommand.run,
   subCommands: {
     generate: generateCommand,
     "generate-templates": generateTemplatesCommand,
