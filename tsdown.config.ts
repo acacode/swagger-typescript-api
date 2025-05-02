8ca1023e17e9e3c2d9092d5b890c8f97202cebe3
Sora Morimoto <sora@morimoto.io>
2025-05-02 20:21:52 +0900

    `tsup` -> `tsdown`
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

---
+++ tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: {
+    lib: "src/index.ts",
+    cli: "index.ts",
+  },
+  format: ["esm", "cjs"],
+  sourcemap: true,
+  target: "node18",
+});
