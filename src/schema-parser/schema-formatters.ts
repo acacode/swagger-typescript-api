441787434bf458f2e20c527dffe5be3beaba6bdf
Vytenis <projektas@gmail.com>
2025-08-08 14:26:15 +0300

    Update schema-formatters.ts
    Fixes #1321

--- src/schema-parser/schema-formatters.ts
+++ src/schema-parser/schema-formatters.ts
@@ -114,6 +114,7 @@ export class SchemaFormatters {
         lodash
           // @ts-expect-error TS(2339) FIXME: Property '_' does not exist on type 'LoDashStatic'... Remove this comment to see the full error message
           ._(description)
+          .replace(/\//g, "") // Remove all forward slashes
           .split(/\n/g)
           .map((part) => part.trim())
           .compact()
