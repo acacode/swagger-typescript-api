8191970c479d24236c80b5068bcab2f5f10597cd
Sora Morimoto <sora@morimoto.io>
2025-08-28 18:07:48 +0900

    Automatically extract enums when using enum names as values (#1383)
    * Automatically extract enums when using enum names as values
    
    * Add changeset
    
    Signed-off-by: Sora Morimoto <sora@morimoto.io>
    
    ---------
    
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

---
+++ tests/spec/enumNamesAsValues/inline.test.ts
@@ -0,0 +1,33 @@
+import * as fs from "node:fs/promises";
+import * as os from "node:os";
+import * as path from "node:path";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import { generateApi } from "../../../src/index.js";
+
+describe("inline enum", async () => {
+  let tmpdir = "";
+
+  beforeAll(async () => {
+    tmpdir = await fs.mkdtemp(path.join(os.tmpdir(), "swagger-typescript-api"));
+  });
+
+  afterAll(async () => {
+    await fs.rm(tmpdir, { recursive: true });
+  });
+
+  test("--enum-names-as-values with inline enum", async () => {
+    await generateApi({
+      fileName: "schema",
+      input: path.resolve(import.meta.dirname, "inline-schema.json"),
+      output: tmpdir,
+      silent: true,
+      enumNamesAsValues: true,
+    });
+
+    const content = await fs.readFile(path.join(tmpdir, "schema.ts"), {
+      encoding: "utf8",
+    });
+
+    expect(content).toMatchSnapshot();
+  });
+});
